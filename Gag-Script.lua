03ddba9d0422e5c298c8b91336687f74c8518d4b
yunoo2 <164744431+yunoo2@users.noreply.github.com>
2025-06-15 12:58:16 +0800

    Create Gag-Script.lua

---
+++ Gag-Script.lua
@@ -0,0 +1,8 @@
+# Roblox Script Loader - yunoo2
+
+This is a GitHub-hosted Roblox loader script using `HttpGet` and `loadstring`.
+
+## 📥 Usage in Roblox
+
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yunoo2/roblox-scripts/main/Gag-Script.lua"))()
